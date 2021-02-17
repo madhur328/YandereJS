052b8786496c7872049abf5fc927beb195353742
mukul <mukul654@gmail.com>
2021-02-17 11:34:49 +0530

    module exports map shortened

--- modules/each.js
+++ modules/each.js
@@ -1,5 +1,9 @@
-const each  =  (list, iteratee) => {
-    for(var i=0; i<Object.keys(list).length; i++){
+const each = (list, iteratee) => {
+    for (var i = 0; i < Object.keys(list).length; i++) {
         iteratee(Object.values(list)[i], Object.keys(list)[i], list)
     }
+}
+
+module.exports = {
+    each
 }
