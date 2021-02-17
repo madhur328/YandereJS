b65d34924f6f0eefec43515d053e43e1f7595f79
mukul <mukul654@gmail.com>
2021-02-17 15:08:41 +0530

    reject

---
+++ modules/reject.js
@@ -0,0 +1,14 @@
+const reject = (list, func) => {
+    var result = []
+    const values = Object.values(list)
+    for (var value of values) {
+        if (!func(value)) {
+            result.push(value)
+        }
+    }
+    return result
+}
+
+module.exports = {
+    reject
+}
