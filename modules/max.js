aba091d07c6634416c5f0cdd6ee2826436148bcd
mukuul <mukul654@gmail.com>
2021-02-19 16:08:50 +0530

    some changes

--- modules/max.js
+++ modules/max.js
@@ -1,16 +1,16 @@
 const max = (list, iteratee = (listelem) => listelem) => {
-    value = Object.values(list)
+    let value = Object.values(list)
     let max = -Infinity
-    let array = [-Infinity]
+    let result = [-Infinity]
     for (let elem of value) {
         let num = iteratee(elem)
         if (num > max) {
             max = num
-            array.pop()
-            array.push(elem)
+            result.pop()
+            result.push(elem)
         }
     }
-    return array.pop()
+    return result.pop()
 }
 
 
