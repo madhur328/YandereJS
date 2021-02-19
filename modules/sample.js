14ecbbd160c701b74e6fbff47eeb07cece4c4dc8
Madhur <madhurgupta328@gmail.com>
2021-02-19 16:26:26 +0530

    sample

---
+++ modules/sample.js
@@ -0,0 +1,19 @@
+const sample = (list, n) => {
+    let result = []
+    let values = Object.values(list)
+    let num = Math.floor(Math.random() * values.length)
+    if (n === "undefined") return values.splice(num, 1)[0]
+    if (n <= 0) { return [] }
+    if (n > values.length) { n = values.length }
+    for (let i = 0; i < n; i++) {
+        result.push(values.splice(num, 1)[0])
+        num = Math.floor(Math.random() * values.length)
+    }
+    return result
+}
+
+module.exports = {
+    sample
+}
+
+console.log(sample([1, 2, 3, 4, 5]))
