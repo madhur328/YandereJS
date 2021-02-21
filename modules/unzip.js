b0f8eaaba1387403d16b23cb34e25aa33a7cb330
mukuul <mukul654@gmail.com>
2021-02-21 16:24:46 +0530

    unzip

---
+++ modules/unzip.js
@@ -0,0 +1,16 @@
+const { max } = require('./max.js');
+const unzip = (arrays) => {
+    let n = max(arrays, n => n.length);
+    let result = [];
+    for (var i = 0; i < arrays.length; i++) {
+        for (var j = 0; j < n.length; j++) {
+            if (!result[j]) { result[j] = [] }
+            result[j].push(arrays[i][j])
+        }
+    }
+    return result;
+}
+
+module.exports = {
+    unzip
+}
