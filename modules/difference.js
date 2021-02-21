7c614bea241f0d5015d2deb1d2baba53ee2bcc4a
mukuul <mukul654@gmail.com>
2021-02-21 15:28:32 +0530

    intersection

--- modules/difference.js
+++ modules/difference.js
@@ -2,7 +2,7 @@ const { without } = require('./without.js');
 const { flatten } = require('./flatten.js');
 
 const difference = (array, ...arrays) => {
-    let array1 = [array, ...arrays];
+    let array1 = Object.values([array, ...arrays]);
     return without(flatten(array1), ...flatten(arrays));
 }
 module.exports = {
