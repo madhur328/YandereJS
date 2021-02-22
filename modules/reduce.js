2752a8450f60c38c2b049a780dbfa48230ce951e
Madhur <madhurgupta328@gmail.com>
2021-02-22 12:19:18 +0530

    reduceRight refactor

--- modules/reduce.js
+++ modules/reduce.js
@@ -1,7 +1,7 @@
 const reduce = (...args) => {
     let [list, iteratee, memo] = args;
+    let entries = Object.entries(list)
     let i = 0;
-    entries = Object.entries(list)
     if (args.length < 3) {
         memo = list[0];
         i++;
@@ -15,15 +15,3 @@ const reduce = (...args) => {
 module.exports = {
     reduce
 }
-
-
-var objectType1 = { one: 1, two: 4, three: 9, four: 16, five: 25 }   // perfect squares of naturals
-var objectType2 = { one: 1, two: 2, three: 3, four: 4, five: 5 }    // naturals
-var objectType3 = { one: 2, two: 28, three: 6, four: 8, five: 10 }   // all even
-var objectType4 = { one: 1, two: 3, three: 5, four: 7, five: 9 }    // all odd
-var objectType5 = [objectType1, objectType2, objectType3, objectType4]
-
-
-const func = elem => elem.two
-var max_obj = reduce(objectType1, (num1, num2) => num1 * num2 ** 0.5)
-console.log(max_obj)
--- modules/reduceRight.js
+++ modules/reduceRight.js
@@ -1,5 +1,12 @@
-const reduceRight = (list, iteratee, memo) => {
-    for (var i = Object.keys(list).length - 1; i >= 0; i--) {
+const reduceRight = (...args) => {
+    let [list, iteratee, memo] = args
+    item_vals = Object.values(list)
+    let i = item_vals.length - 1    //last elem index
+    if (args.length < 3) {
+        memo = item_vals[i]
+        i--
+    }
+    for (i; i >= 0; i--) {
         memo = iteratee(memo, Object.values(list)[i], Object.keys(list)[i], list)
     }
     return memo;
