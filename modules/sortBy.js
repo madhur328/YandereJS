a98c8974bf3aab54616062dbb2513ec5bbed1d0d
Madhur <madhurgupta328@gmail.com>
2021-02-24 14:32:57 +0530

    sortBy refactor

--- modules/sortBy.js
+++ modules/sortBy.js
@@ -1,43 +1,22 @@
 const sortBy = (list, iteratee) => {
     if (typeof iteratee === "string") { let str = iteratee; iteratee = elem => elem[str] }
-    item_vals = Object.values(list)
-    let max_value = iteratee(item_vals[0])
-    let min_value = iteratee(item_vals[0])
-    let result_arr = [item_vals[0]]
-    let result_arr_copy = result_arr
-    for (let i = 1; i < item_vals.length; i++) {
-        let value = iteratee(item_vals[i])
-        if (value > max_value) {
-            max_value = value
-            result_arr.push(item_vals[i])
-            result_arr_copy = result_arr
-        } else {
-            for (let j = result_arr_copy.length - 1; j >= 0; j--) {
-                if (value > iteratee(result_arr[j])) {
-                    result_arr.splice(j + 1, 0, item_vals[i])
-                    break;
-                }
-                else if (value < min_value) {
-                    min_value = value
-                    result_arr.splice(0, 0, item_vals[i])
-                }
+    let item_vals = Object.values(list)
+    let temp = null
+    for (let i = item_vals.length - 2; i >= 0; i--)
+        for (let j = 0; j <= i; j++) {
+            if (iteratee(item_vals[j]) > iteratee(item_vals[j + 1])) {
+                temp = item_vals[j + 1]
+                item_vals[j + 1] = item_vals[j]
+                item_vals[j] = temp
             }
-            result_arr_copy = result_arr
         }
-    }
-    return result_arr
+    return item_vals
 }
 
 module.exports = {
     sortBy
 }
 
-// let stooges = [{ name: 'moe', age: 40 }, { name: 'larry', age: 50 }, { name: 'curly', age: 60 }];
-// let sortByOutput = sortBy(stooges, function (elem) { return elem.name })
-// console.log(sortByOutput)
-
-
-let stooges = [{ name: 'moe', age: 40 }, { name: 'larry', age: 50 }, { name: 'curly', age: 60 }];
-let sortByOutput = sortBy(stooges, 'name')
+let stooges = [{ name: 'moe', age: 55 }, { name: 'larry', age: 50 }, { name: 'curly', age: 60 }];
+let sortByOutput = sortBy(stooges, 'age')
 console.log(sortByOutput)
-
