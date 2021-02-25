80a28d880a924e19902cdd9dca855081a2cf11cb
Madhur <madhurgupta328@gmail.com>
2021-02-25 11:22:19 +0530

    array method 'first' refactor and test conditions

--- modules/first.js
+++ modules/first.js
@@ -1,11 +1,51 @@
-const first = (array, n) => {
-    if (!n && n !== 0) { return array[0] }
-    else {
-        let result = array.splice(0, n);
-        return result;
+const first = (...args) => {
+    let [array, n] = args
+    if (array && array.length) {
+        return args.length > 1 ? array.splice(0, n) : array[0]
     }
 }
 
 module.exports = {
     first
 }
+
+
+// Test 1   no arguments
+let first_output = first()
+console.log(first_output)   // expected output: undefined
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 2  single non array argument
+first_output = first(1)
+console.log(first_output)  // expected output: undefined
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 3  two arguments; first is not an array
+first_output = first(1, 2)
+console.log(first_output)  // expected output: undefined
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 4  multiple arguments; first is non array
+first_output = first(1, 2, [5, 7])
+console.log(first_output)  // expected output: undefined
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 5  single array argument
+first_output = first([5, 7])
+console.log(first_output)  // expected output: first element of the array
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 6  two args; array argument and n < array length
+first_output = first([1, 2, 3, 4, 5, 6], 3)
+console.log(first_output)  // expected output: first n elements of array
+
+console.log("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
+
+// Test 7  two args; array argument and n > array length
+first_output = first([1, 2, 3, 4, 5, 6], 15)
+console.log(first_output)  // expected output: complete array
