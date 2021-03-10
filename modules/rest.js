d25320d3bb64de72cf86e29427b629df80b68102
Madhur <madhurgupta328@gmail.com>
2021-03-10 16:14:24 +0530

    rest.js update

--- modules/rest.js
+++ modules/rest.js
@@ -1,7 +1,16 @@
-const rest = (array, index = 1) => {
-    let result = array.splice(index)
-    return result;
+const rest = (iterable, index = 1) => {
+    if (iterable) {
+        if (iterable.length && typeof iterable.length === "number") {
+            // iterable may be array or string type
+            if (typeof iterable === "string") return iterable.slice(index).split('')
+            return iterable.slice(index)
+        } return []
+    }
 }
+
 module.exports = {
     rest
 }
+
+let rest_output = rest([1, 2, 3, 4, 5, 6])
+console.log(rest_output)
